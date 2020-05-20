f7c13e22df6f601af8966650d055c120b433bf72
lozitskiys <lozitskiys@gmail.com>
2020-05-20 17:49:37 +0300

    fosrest bundle

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -2,18 +2,29 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use FOS\RestBundle\Controller\AbstractFOSRestController;
+use FOS\RestBundle\Controller\Annotations as Rest;
 use Symfony\Component\Routing\Annotation\Route;
 
-class LuckyController extends AbstractController
+class LuckyController extends AbstractFOSRestController
 {
     /**
-     * @Route("/lucky/number")
+     * @Route("/lucky/number", name="lnumber")
      */
     public function number()
     {
         $number = random_int(0, 100);
 
         return $this->json($number);
+    }
+
+    /**
+     * @Rest\Post("/list", name="testlist")
+     */
+    public function list()
+    {
+        $number = random_int(0, 100);
+
+        return $this->json([$number]);
     }
 }
