4ce178cf3ee87bf220113f460a7cb19a78aff8d5
lozitskiys <lozitskiys@gmail.com>
2020-05-20 15:32:29 +0300

    composer require annotations

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -14,8 +14,6 @@ class LuckyController extends AbstractController
     {
         $number = random_int(0, 100);
 
-        return $this->render('lucky-number.html.twig', [
-            'number' => $number,
-        ]);
+        return $this->json($number);
     }
 }
